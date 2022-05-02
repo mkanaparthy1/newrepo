d331175af889bb0becd54d20aa40498b67b5c2b5
mkanaparthy1 <103694453+mkanaparthy1@users.noreply.github.com>
2022-05-01 22:34:10 -0700

    Update TravelDistance.java

--- TravelDistance.java
+++ TravelDistance.java
@@ -1,29 +1,8 @@
 public class TravelDistance {
     private static final String ERR_MSG = "Invalid input!";
 
-    /**
-     * Given a distance of `n` feet, you can either take a step
-     *  (which covers 1 foot) or a leap (which covers 3 feet). This
-     *  method should calculate the different number of ways you can
-     *  step/leap to reach the distance of `n` feet.
-     *
-     * If `n` = 0, then this method should return `1`.
-     *
-     * This method should gracefully handle negative numbers (by
-     *  throwing an exception with message `ERR_MSG`).
-     *
-     * For example, if `n = 4`, then there are 3 ways to reach
-     *  the distance of 4:
-     *  1. step + step + step + step = 1 + 1 + 1 + 1
-     *  2. step + leap = 1 + 3
-     *  3. leap + step = 3 + 1
-     *
-     * HINT: You might want to create a helper method just for the
-     *  recursive part.
-     *
-     * @param n the distance (in feet) to reach
-     * @return the number of ways to step/leap to `n`
-     */
+   aram n the distance (in feet) to reach
+     
     public static int calculateWays(int n) throws Exception {
 
         if (n < 0) {
