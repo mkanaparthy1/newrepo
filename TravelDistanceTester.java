03b0b5550e06fece44b8204207ee5d6fd3e7dacc
mkanaparthy1 <103694453+mkanaparthy1@users.noreply.github.com>
2022-05-01 22:33:44 -0700

    Create TravelDistanceTester.java

---
+++ TravelDistanceTester.java
@@ -0,0 +1,33 @@
+public class TravelDistanceTester {
+    
+    public static void main(String[] args) {
+
+        TravelDistance traveldistance = new TravelDistance();
+        try {
+            System.out.println(traveldistance.calculateWays(3));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // The number of ways to reach distance of 4 should be 3.
+        try {
+            if (TravelDistance.calculateWays(4) != 3) {
+                System.out.println("TEST 1 FAILED.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        // The number of ways to reach distance of 5 should be 5.
+        try {
+            if (TravelDistance.calculateWays(5) != 4) {
+                System.out.println("TEST 2 FAILED.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+      
+    }
+}
